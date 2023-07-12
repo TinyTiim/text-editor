166d7f78b3cb48420be7b8ac3953aa0be8579595
unknown <sophia.palomo12@gmail.com>
2023-07-11 18:27:16 -0700

    attempting to fix 404 error

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,37 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt;
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  
+  event.preventDefault();
+  
+  deferredPrompt = event;
+  // Show the install button
+  butInstall.style.display = 'block';
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Click event handler for the install button
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    
+    deferredPrompt.prompt();
+    
+    const choiceResult = await deferredPrompt.userChoice;
+    if (choiceResult.outcome === 'accepted') {
+      console.log('PWA installation accepted.');
+    } else {
+      console.log('PWA installation dismissed.');
+    }
+  
+    deferredPrompt = null;
+   
+    butInstall.style.display = 'none';
+  }
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Event handler for the appinstalled event
+window.addEventListener('appinstalled', (event) => {
+  console.log('PWA installed successfully.');
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,34 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'My App',
+        short_name: 'App',
+        description: 'My Progressive Web App',
+        background_color: '#ffffff',
+        theme_color: '#31a9e1',
+        icons: [
+          // icon configuration here
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
       ],
     },
   };
